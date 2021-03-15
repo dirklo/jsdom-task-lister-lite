82153e1f294c0018de9b056330a799bdf6679456
Rick <dirklo@gmail.com>
2021-03-15 18:44:46 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const list = document.querySelector('#tasks');
+  const form = document.querySelector('#create-task-form');
+  const text_field = document.querySelector('#new-task-description');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const new_li = document.createElement('li');
+    new_li.innerText = text_field.value;
+    list.append(new_li);
+    text_field.value = "";
+    e.target.reset();
+  });
 });
